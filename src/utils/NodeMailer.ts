adc6791e362a41ed0d5cbec8450d11c863bbb631
Yasir Oladimeji <yasiroyebo@gmail.com>
2025-06-03 14:00:22 +0100

    feat:complete email sender using sendGrid

--- src/utils/NodeMailer.ts
+++ src/utils/NodeMailer.ts
@@ -1,12 +1,14 @@
 import * as nodeMailer from 'nodemailer';
 import * as SendGrid from 'nodemailer-sendgrid-transport';
+import { getEnvironmentVariables } from '../environments/environment';
 
 export class NodeMailer{
     private static initiateTransport(){
-        return nodeMailer.createTransport(SendGrid({
-            auth: {
-                api_key: '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'
-            }
+        return nodeMailer.createTransport(
+            SendGrid({
+                auth: {
+                    api_key: getEnvironmentVariables().sendgrid_api_key
+                }
         }));
     }
 
