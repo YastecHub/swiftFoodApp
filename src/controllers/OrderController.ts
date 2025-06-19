416acc300f6152ecf8154ec214bb64d7176cc3f2
Yasir Oladimeji <yasiroyebo@gmail.com>
2025-06-19 13:14:38 +0100

    feat: added pagination to orders

--- src/controllers/OrderController.ts
+++ src/controllers/OrderController.ts
@@ -45,12 +45,33 @@ export class OrderController {
 
     static async getUserOrders(req, res, next) {
         const user_id = req.user.aud;
+        const perPage = 5;
+        const currentPage = parseInt(req.query.page) || 1;
+        const prevPage = currentPage == 1 ? null : currentPage - 1;
+        let nextPage = currentPage + 1;
         try {
+            const orders_doc_count = await Order.countDocuments({ user_id: user_id });
+            const totalPages = Math.ceil(orders_doc_count/perPage);
+            if(totalPages == 0 || totalPages == currentPage) {
+                nextPage = null;
+            } 
+            if(totalPages < currentPage) {
+                throw('No more Orders available');
+            }
             const orders = await Order.find({ user_id }, {user_id: 0, __v: 0})
+                .skip((perPage * currentPage) - perPage)
+                .limit(perPage)
                 .sort({ 'created_at': -1 })
                 .populate('restaurant_id')
-                .exec(); 
-            res.send(orders);
+                .exec();
+            res.json({
+                orders,
+                perPage,
+                currentPage,
+                prevPage,
+                nextPage,
+                totalPages
+            });
         } catch(e) {
             next(e);
         }
